586f07a672e809cc9dc1a00a9d42e5e0bbd2674c
VeryRoomy <veryroomy@gmail.com>
2016-02-06 19:46:11 -0500

    viv

--- main.js
+++ main.js
@@ -67,8 +67,9 @@
                     var button = document.createElement("button");
                     var button2 = document.createElement("button");
                     button.textContent = "THIS IS A BUTTON";
-                    button.onclick = "send(game:host);";
-                    button2.onclick = "send(game:client)";
+                    button.setAttribute( "onClick", "javascript: send(game:host);" );
+
+                    button2.setAttribute( "onClick", "javascript: send(game:client);" );
                     button2.textContent = "THIS IS A BUTTON TOO";
                     button.className += " joinButton btn btn-success";
                     button2.className += " joinButton btn btn-success";
